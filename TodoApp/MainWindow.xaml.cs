7c7d44dd6deb859323cfba950f6bc729fa0620a1
robert <robert89221@outlook.com>
2023-11-24 09:52:06 +0100

    Clean up and add some comments

--- TodoApp/MainWindow.xaml.cs
+++ TodoApp/MainWindow.xaml.cs
@@ -1,86 +1,80 @@
-﻿using Microsoft.Win32;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.ComponentModel.Design.Serialization;
+﻿
+//  ToDo app
+
+using Microsoft.Win32;
 using System.IO;
-using System.Security.Cryptography;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace TodoApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        //internal TodoList list;
-        private List<TodoItem> list;
+        private List<TodoItem> TodoList;
+
 
         public MainWindow()
         {
             InitializeComponent();
 
-            list = new List<TodoItem>();
-
-            lv_List.ItemsSource = list;
-
+            TodoList = new List<TodoItem>();
+            lv_List.ItemsSource = TodoList;
             dp_Date.SelectedDate = DateTime.Today;
         }
 
 
         private void bt_ClearList_Click(object sender, RoutedEventArgs e)
         {
-            list.Clear();
+            //  Clear the list and reset filtering
+
+            TodoList.Clear();
             tb_Search.Text = "";
             lv_List.Items.Filter = null;
-
             lv_List.Items.Refresh();
         }
 
 
         private void bt_Delete_Click(object sender, RoutedEventArgs e)
         {
-            foreach (TodoItem item in lv_List.SelectedItems)    list.Remove(item);
+            //  Remove selected items
+
+            foreach (TodoItem item in lv_List.SelectedItems)    TodoList.Remove(item);
             lv_List.Items.Refresh();
         }
 
 
         private void bt_MarkDone_Click(object sender, RoutedEventArgs e)
         {
-            //  markera först alla som färdiga, sedan som ej färdiga
+            //  Mark selected items as done, then mark them all as not done
 
             var query = from TodoItem item in lv_List.SelectedItems select item;
-            var newState = query.All(item => item.IsDone) ? false : true;
+            var newState = query.All(item  =>  item.IsDone) ? false : true;
 
             foreach (var item in query)    item.IsDone = newState;
 
-            list.Sort();
+            TodoList.Sort();
             lv_List.Items.Refresh();
         }
 
 
         private void bt_NewTodo_Click(object sender, RoutedEventArgs e)
         {
-            var dt = DateOnly.FromDateTime(dp_Date.SelectedDate ?? DateTime.Today);
-            list.Add(new TodoItem(tb_Description.Text, tb_Category.Text, dt));
+            //  Create new ToDo item using selected data, or today's date if no date is selected yet
 
-            lv_List.SelectedItem = list.Last();
-            list.Sort();
+            var date = DateOnly.FromDateTime(dp_Date.SelectedDate ?? DateTime.Today);
+            TodoList.Add(new TodoItem(tb_Description.Text, tb_Category.Text, date));
+
+            lv_List.SelectedItem = TodoList.Last();
+            TodoList.Sort();
             lv_List.Items.Refresh();
         }
 
 
         private void lv_List_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //  If we select a single item, update the category, description, and date accordingly
+
             if (lv_List.SelectedItems.Count == 1)
             {
                 tb_Category.Text = (lv_List.SelectedItems[0] as TodoItem)!.Category;
@@ -96,23 +90,24 @@ namespace TodoApp
 
         private void bt_Search_Click(object sender, RoutedEventArgs e)
         {
+            //  Split the search text into an array of words
+            //  If any of the words appear in the category or description, we include the result
+
             var words = tb_Search.Text.Trim().ToLower().Split();
 
-            if (words[0] == "")
-            {
-                lv_List.Items.Filter = null;
-            }
-            else
-            {
-                lv_List.Items.Filter = item => words.Any(((item as TodoItem)!.Description + " " + (item as TodoItem)!.Category).ToLower().Contains);
-            }
+            //  Disable filtering if the search text is empty, or the ListView will stop functioning
+
+            if (words[0] == "")    lv_List.Items.Filter = null;
+            else                   lv_List.Items.Filter = item  =>  words.Any(((item as TodoItem)!.Description + " " + (item as TodoItem)!.Category).ToLower().Contains);
 
             lv_List.Items.Refresh();
         }
 
         private void bt_SaveList_Click(object sender, RoutedEventArgs e)
         {
-            if (list.Count == 0)    return;
+            //  Do not bother opening the file dialog if the list is empty
+
+            if (TodoList.Count == 0)    return;
 
             var dlg = new SaveFileDialog();
             dlg.Filter = "ToDo List|*.tod|All Files|*.*";
@@ -121,9 +116,12 @@ namespace TodoApp
 
             using (var stream = new StreamWriter(dlg.OpenFile()))
             {
-                stream.WriteLine(list.Count);
+                //  If the file stream was correctly opened, save the number of items in the list, followed by each of the items
+                //  with all fields formatted as strings on separate lines
 
-                foreach (var item in list)
+                stream.WriteLine(TodoList.Count);
+
+                foreach (var item in TodoList)
                 {
                     stream.WriteLine(item.IsDone);
                     stream.WriteLine(item.Date);
@@ -145,6 +143,8 @@ namespace TodoApp
 
             using (var stream = new StreamReader(dlg.OpenFile()))
             {
+                //  Read the number of items in the list file, and recreate the whole list one item at a time
+
                 int count = Convert.ToInt32(stream.ReadLine());
                 var newList = new List<TodoItem>();
 
@@ -158,8 +158,10 @@ namespace TodoApp
                     newList.Add(new TodoItem(desc!, cat!, date, done));
                 }
 
-                list = newList;
-                lv_List.ItemsSource = list;
+                //  Replace with the newly read list, reset filtering, and refresh the ListView
+
+                TodoList = newList;
+                lv_List.ItemsSource = TodoList;
                 lv_List.Items.Filter = null;
                 tb_Search.Text = "";
                 lv_List.Items.Refresh();
--- TodoApp/TodoItem.cs
+++ TodoApp/TodoItem.cs
@@ -1,31 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace TodoApp
 {
-    internal class TodoItem : IComparable<TodoItem>
+    //  A class representing a todo-item
+
+    internal class TodoItem:IComparable<TodoItem>
     {
         public string Description { get; set; }
         public string Category { get; set; }
         public DateOnly Date { get; set; }
         public string DateString
         {
+            //  The DateString property is used to enhance the date formatting when shown in the ListView
+
             get
             {
-                if (IsDone) return "Done";
-                else if (Date == DateOnly.FromDateTime(DateTime.Today)) return "Today";
-                else return Date.ToString();
+                if (IsDone)                                                return "Done";
+                else if (Date == DateOnly.FromDateTime(DateTime.Today))    return "Today";
+                else                                                       return Date.ToString();
             }
         }
         public bool IsDone { get; set; }
 
-        public TodoItem(string d, string c, DateOnly date, bool done = false) => (Description, Category, Date, IsDone) = (d, c, date, done);
+        public TodoItem(string d, string c, DateOnly date, bool done = false)  =>  (Description, Category, Date, IsDone) = (d, c, date, done);
         
         public int CompareTo(TodoItem? that)
         {
+            //  Implement IComparable to control the sorting order in the ListView
+
             var a = $"{this.IsDone}{this.Date}{this.Category}";
             var b = $"{that!.IsDone}{that!.Date}{that!.Category}";
             return a.CompareTo(b);
